f4e99236827710c9ed92d1d83325af28d2292ff6
Caleb Brewer <calebthebrewer@gmail.com>
2014-03-11 00:16:17 -0500

    module pattern?

--- jsenv.js
+++ jsenv.js
@@ -1,10 +1,38 @@
+var jsenvy = (function(window, document, undefined) {
+	var filesLoaded = [],
+		self = this;
+	
+	function loadFile(file, callback) {
+		if (self.filesLoaded.indexOf(file) >= 0) return callback('We already got this for you.');
+		
+		var script = document.createElement('script');
+		script.src = file;
+		script.asynch = true;
+		script.onreadystatechange = script.onload = function() {
+			var state = script.readyState;
+			if (!callback.done && (!state || /loaded|complete/.test(state))) {
+				self.filesLoaded.push(file);
+				callback.done = true;
+				callback('The following has been loaded: ' + file);
+			}
+		};
+	}
+	
+	self.load = function(file) {
+		loadFile(file, function(message) {
+			alert(message);
+		});
+	};
+	
+	return self;
+}(window, this));
+
+/**
 requirejs.config({
 	appDir: ".",
 	baseUrl: "libraries",
 	paths: {
-		/* Load jquery from google cdn. On fail, load local file. */
 		'jquery': ['//code.jquery.com/jquery-2.1.0'],
-		/* Load bootstrap from cdn. On fail, load local file. */
 		'bootstrap': ['//netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.', 'libs/bootstrap-min'],
 		'backbone': ['//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min']
 	}
@@ -26,3 +54,5 @@ document.getElementById('libraryForm').onsubmit = function(e) {
 		alert('library could not be loaded...');
 	}
 };
+
+**/
