d7aad60e854cebd7a50b8175f69227f95b8d83d5
calebthebrewer <calebthebrewer@gmail.com>
2014-03-26 11:57:41 -0400

    removing require and adding working console, also hiding config space

--- jsenvy.js
+++ jsenvy.js
@@ -1,6 +1,7 @@
 var jsenvy = (function() {
 	//keep track of
 	var filesLoaded = [];
+	var cdnjsLibraries = preloadCdnjs();
 
 	function get(url, callback) {
 		var xhr;
@@ -14,14 +15,14 @@ var jsenvy = (function() {
 					"MSXML2.XmlHttp.3.0",
 					"MSXML2.XmlHttp.2.0",
 					"Microsoft.XmlHttp"];
-
+					
 			for (var i = 0, len = versions.length; i < len; i++) {
 				try {
 					xhr = new ActiveXObject(versions[i]);
 					break;
 				} catch(e) {
 				}
-			} // end for
+			}
 		}
 		xhr.onreadystatechange = ensureReadiness;
 		function ensureReadiness() {
@@ -31,8 +32,6 @@ var jsenvy = (function() {
 			if (xhr.status !== 200) {
 				return;
 			}
-
-			// all is well
 			if (xhr.readyState === 4) {
 				callback(xhr);
 			}
@@ -41,6 +40,12 @@ var jsenvy = (function() {
 		xhr.open('GET', url, true);
 		xhr.send('');
 		return xhr;
+	}
+	
+	function preloadCdnjs() {
+		var request = get("http://api.cdnjs.com/libraries", function(data) {
+			cdnjsLibraries = JSON.parse(data.response).results;
+		});
 	}
 
 
@@ -69,60 +74,130 @@ var jsenvy = (function() {
 				}
 			};
 		},
-		searchCdnjs : function(query, callback) {
-			var request = get("http://api.cdnjs.com/libraries?search=" + query.replace(/\s/g, "+"), function(data) {
-				callback(JSON.parse(data.response));
-			});
+		searchCdnjs: function(query) {
+			var results = [];
 			
-			return request;
+			for (var i = 0; i < cdnjsLibraries.length; i++) {
+				if (cdnjsLibraries[i].name.indexOf(query) > -1) {
+					results.push(cdnjsLibraries[i]);
+				}
+			}
+			
+			return results;
+		},
+		getMethods: function(object) {
+			var options = [];
+			for (var things in object) {
+				if (["object", "function"].indexOf(typeof object[things]) > -1) {
+					options.push(things);
+				}
+			}
+			return options;
+		},
+		toggleHideable: function(element) {
+			if (element.nextElementSibling.getAttribute("data-hidden") != "true") {
+				element.nextElementSibling.classList.add("hidden");
+				element.nextElementSibling.setAttribute("data-hidden", "true");
+			} else {
+				element.nextElementSibling.classList.remove("hidden");
+				element.nextElementSibling.setAttribute("data-hidden", "false");
+			}
 		}
 	};
 })();
 
-var jsenvyUi = {};
-
-jsenvyUi.load = function(file) {
-	jsenvy.load(file, function(success, message) {
-		if (success) {
-			document.getElementById("libraryName").value = "";
-			document.getElementById("librarySuggestions").innerHTML = "";
-			
-			var li = document.createElement('li');
-			li.innerHTML = file;
-			document.getElementById('loadedLibraries').appendChild(li);
-		} else {
-			alert(message);
-		}
-	});
-};
-
-document.getElementById('libraryForm').onsubmit = function(e) {
-	//set the table
-	e.preventDefault();
-	var self = this;
-
-	jsenvyUi.load(this.libraryName.value);
-};
-
-jsenvyUi.searchRequest = null;
-document.getElementById('libraryName').onkeyup = function(e) {
-	if (this.value.length > 2) {
-		//clear current suggestions
-		if (jsenvyUi.searchRequest) jsenvyUi.searchRequest.abort();
-		document.getElementById("librarySuggestions").innerHTML = "";
-		//get the new stuff
-		jsenvyUi.searchRequest = jsenvy.searchCdnjs(this.value, function(data) {
-			if (!data.results) return console.log("No results?");
-			
-			for (var i = 0; i < data.results.length; i++) {
-				var li = document.createElement("li");
-				li.innerHTML = data.results[i].name;
-				li.setAttribute("data-src", data.results[i].latest);
-				li.onclick = function() {
-					jsenvyUi.load(this.getAttribute("data-src"));
-				};
-				document.getElementById("librarySuggestions").appendChild(li);
+var jsenvyUi = (function() {
+	var jsenvyUi = {};
+	
+	jsenvyUi.load = function(file) {
+		jsenvy.load(file, function(success, message) {
+			if (success) {
+				document.getElementById("libraryName").value = "";
+				document.getElementById("librarySuggestions").innerHTML = "";
+				
+				var li = document.createElement('li');
+				li.innerHTML = file;
+				document.getElementById('loadedLibraries').appendChild(li);
+			} else {
+				alert(message);
 			}
 		});
-	}
-};
+	};
+	
+	jsenvyUi.enableHideables = function() {
+		var hideables = document.getElementsByClassName("hideable");
+		
+		for (var i = 0; i < hideables.length; i++) {
+			hideables[i].onclick = function(e) {
+				jsenvy.toggleHideable(this);
+			}
+		}
+	};
+	
+	document.getElementById('libraryName').onkeyup = function() {
+		if (this.value.length < 1) {
+			document.getElementById("librarySuggestions").innerHTML = "";
+			return;
+		}
+		
+		document.getElementById("librarySuggestions").innerHTML = "";
+		//get the new stuff
+		var results = jsenvy.searchCdnjs(this.value);
+		if (!results.length) {
+			document.getElementById("suggestions-help").innerHTML = "No results.";
+			return;
+		}
+		
+		for (var i = 0; i < results.length; i++) {
+			document.getElementById("suggestions-help").innerHTML 
+				= "To load a cdnjs library, select it from the list.";
+			var li = document.createElement("li");
+			li.innerHTML = results[i].name;
+			li.setAttribute("data-src", results[i].latest);
+			li.onclick = function() {
+				jsenvyUi.load(this.getAttribute("data-src"));
+			};
+			document.getElementById("librarySuggestions").appendChild(li);
+		}
+	};
+	
+	document.getElementById('loadFromUrl').click = function(e) {
+		jsenvyUi.load(document.getElementById("libraryName").value);
+	};
+
+	jsenvyUi.enableHideables();
+	
+	return jsenvyUi;
+})();
+
+var jsConsole = (function(id) {
+	//cache the money
+	var $console = document.getElementById(id),
+		$form = $console.getElementsByTagName('form')[0],
+		$input = $form.input,
+		$history = $console.getElementsByClassName('history')[0];
+	//execute the codez
+	$form.onsubmit = function(e) {
+		e.preventDefault();
+		
+		var input = document.createElement("span")
+		input.classList.add("input");
+		input.classList.add("col-md-6");
+		input.innerHTML = $input.value;
+		
+		var output = document.createElement("span")
+		output.classList.add("output");
+		output.classList.add("col-md-6");
+		output.innerHTML = eval($input.value);
+		
+		var li = document.createElement("li");
+		li.appendChild(input)
+		li.appendChild(output);
+		//clean up
+		$history.appendChild(li);
+		$input.value = "";
+	};
+
+	
+})("console");
+
